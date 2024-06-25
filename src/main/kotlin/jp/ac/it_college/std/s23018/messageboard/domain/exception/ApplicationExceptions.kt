2b2986fa1b1c897d614826b7c54f3c53c5db39d7
itc-s23018 <s23018@std.it-college.ac.jp>
2024-06-25 14:05:29 +0900

    最初から作り直し

--- src/main/kotlin/jp/ac/it_college/std/s23018/messageboard/domain/exception/ApplicationExceptions.kt
+++
@@ -1,7 +0,0 @@
-package jp.ac.it_college.std.s23018.messageboard.domain.exception
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(HttpStatus.CONFLICT)
-class EmailAlreadyInUseException(override val message: String ) : Exception()
